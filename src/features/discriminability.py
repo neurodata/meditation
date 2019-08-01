fc93377d83a778d296bff7c93e56e62df375c863
rflperry <rflperry@gmail.com>
2019-08-01 08:42:06 -0400

    debugged gcca code, matches MATLAB up until sign flip

--- src/features/discriminability.py
+++ src/features/discriminability.py
@@ -1,23 +1,184 @@
 #-*- coding: utf-8 -*-
 
-__author__ = 'Ronan Perry'
+__author__ = 'Alex'
 
+# Taken from https://github.com/neurodata/graphutils/blob/master/graphutils/utils.py
 
+#%%
+from pathlib import Path
+
+from sklearn.metrics import euclidean_distances
+from sklearn.utils import check_X_y
 import numpy as np
-from scipy import linalg,stats
-from scipy.sparse.linalg import svds
-from sklearn.preprocessing import normalize
 
-import tqdm
+KEYWORDS = ["sub", "ses"]
 
-def _matrix_distance(A1,A2):
-    # Flattent two matrices and l2 norm
-    
-def _intra_delta(i,i2,t,t2):
-    
-def _inter_delta(i,t,t2):
 
-def _reliability(i,t,t2):
+def is_graph(filename, atlas="", suffix=""):
+    """
+    Check if `filename` is a ndmg graph file.
     
-def discriminability():
+    Parameters
+    ----------
+    filename : str or Path
+        location of the file.
     
+    Returns
+    -------
+    bool
+        True if the file has the ndmg naming convention, else False.
+    """
+
+    if atlas:
+        atlas = atlas.lower()
+        KEYWORDS.append(atlas)
+
+    if suffix:
+        if not suffix.startswith("."):
+            suffix = "." + suffix
+
+    correct_suffix = Path(filename).suffix == suffix
+    correct_filename = all(i in str(filename) for i in KEYWORDS)
+    return correct_suffix and correct_filename
+
+
+def filter_graph_files(file_list, return_bool=False, **kwargs):
+    """
+    Generator. 
+    Check if each file in `file_list` is a ndmg edgelist,
+    yield it if it is.
+    
+    Parameters
+    ----------
+    return_bool : bool
+        if True, return a boolearn that says whether graph files exist in the directory.
+    file_list : iterator
+        iterator of inputs to the `is_graph` function.
+    """
+    if return_bool:
+        has_graphs = any(is_graph(x, **kwargs) for x in file_list)
+        return has_graphs
+    for filename in file_list:
+        if is_graph(filename, **kwargs):
+            yield (filename)
+
+
+def discr_stat(
+    X, Y, dissimilarity="euclidean", remove_isolates=True, return_rdfs=False
+):
+    """
+    Computes the discriminability statistic.
+    Parameters
+    ----------
+    X : array, shape (n_samples, n_features) or (n_samples, n_samples)
+        Input data. If dissimilarity=='precomputed', the input should be the dissimilarity matrix.
+    Y : 1d-array, shape (n_samples)
+        Input labels.
+    dissimilarity : str, {"euclidean" (default), "precomputed"}
+        Dissimilarity measure to use:
+        - 'euclidean':
+            Pairwise Euclidean distances between points in the dataset.
+        - 'precomputed':
+            Pre-computed dissimilarities.
+    remove_isolates : bool, optional, default=True
+        Whether to remove data that have single label.
+    return_rdfs : bool, optional, default=False
+        Whether to return rdf for all data points.
+    Returns
+    -------
+    stat : float
+        Discriminability statistic. 
+    rdfs : array, shape (n_samples, max{len(id)})
+        Rdfs for each sample. Only returned if ``return_rdfs==True``.
+    """
+    check_X_y(X, Y, accept_sparse=True)
+
+    uniques, counts = np.unique(Y, return_counts=True)
+    if (counts != 1).sum() <= 1:
+        msg = "You have passed a vector containing only a single unique sample id."
+        raise ValueError(msg)
+    if remove_isolates:
+        idx = np.where(np.isin(Y, uniques[counts != 1]))
+        print(idx)
+        labels = Y[idx]
+
+        if dissimilarity == "euclidean":
+            X = X[idx]
+        else:
+            X = X[np.ix_(idx, idx)]
+    else:
+        labels = Y
+
+    if dissimilarity == "euclidean":
+        dissimilarities = euclidean_distances(X)
+    else:
+        dissimilarities = X
+
+    rdfs = _discr_rdf(dissimilarities, labels)
+    stat = np.nanmean(rdfs)
+
+    if return_rdfs:
+        return stat, rdfs
+    else:
+        return stat
+
+
+def _discr_rdf(dissimilarities, labels):
+    """
+    A function for computing the reliability density function of a dataset.
+    Parameters
+    ----------
+    dissimilarities : array, shape (n_samples, n_features) or (n_samples, n_samples)
+        Input data. If dissimilarity=='precomputed', the input should be the 
+        dissimilarity matrix.
+    labels : 1d-array, shape (n_samples)
+        Input labels.
+    Returns
+    -------
+    out : array, shape (n_samples, max{len(id)})
+        Rdfs for each sample. Only returned if ``return_rdfs==True``.
+    """
+    check_X_y(dissimilarities, labels, accept_sparse=True)
+
+    rdfs = []
+    for i, label in enumerate(labels):
+        di = dissimilarities[i]
+
+        # All other samples except its own label
+        idx = labels == label
+        Dij = di[~idx]
+
+        # All samples except itself
+        idx[i] = False
+        Dii = di[idx]
+
+        rdf = [1 - ((Dij < d).sum() + 0.5 * (Dij == d).sum()) / Dij.size for d in Dii]
+        rdfs.append(rdf)
+
+    out = np.full((len(rdfs), max(map(len, rdfs))), np.nan)
+    for i, rdf in enumerate(rdfs):
+        out[i, : len(rdf)] = rdf
+
+    return out
+
+
+def replace_doc(value):
+    """
+    Decorator for changing docstring of a function.
+    
+    Parameters
+    ----------
+    value : str
+        docstring to change to.
+    
+    Returns
+    -------
+    func
+        wrapper function.
+    """
+
+    def _doc(func):
+        func.__doc__ = value
+        return func
+
+    return _doc
--- src/features/gcca.py
+++ src/features/gcca.py
@@ -8,17 +8,22 @@ from scipy import linalg,stats
 from scipy.sparse.linalg import svds
 from sklearn.preprocessing import normalize
 
-import tqdm
+from tqdm import tqdm
 
 
 def _preprocess(x):
-    x2 = stats.zscore(x,axis=1)
-    x2 -= np.mean(x2,axis=0)
-    return x2
+    # Mean along rows using sample mean and sample std
+    x2 = stats.zscore(x,axis=1,ddof=1) 
+    # Mean along columns
+    mu = np.mean(x2,axis=0)
+    x2 -= mu
+    return(x2)
 
 def gcca(data, rank_tolerance=None, n_components=None):
     n = data[0].shape[0]
-
+    
+    data = [_preprocess(x) for x in data]
+    
     Uall = []
     Sall = []
     Vall = []
@@ -26,30 +31,32 @@ def gcca(data, rank_tolerance=None, n_components=None):
     
     for x in tqdm(data):
         # Preprocess
-        x = _preprocess(x)
         x[np.isnan(x)] = 0
+        
+        temp['X'] = x
 
         # compute the SVD of the data
-        v,s,ut = linalg.svd(x.T, full_matrices=False)
+        u,s,vt = linalg.svd(x, full_matrices=False)
 
         Sall.append(s)
-        Vall.append(v.T)
+        Vall.append(vt.T)
         # Dimensions to reduce to
         if rank_tolerance:
-            rank = sum(S > rank_tolerance)
+            rank = sum(s > rank_tolerance)
         else:
             rank = n_components
+            
         ranks.append(rank)
-        ut = ut.T[:,:rank]
-        Uall.append(ut)
-
+        u = u[:,:rank]
+        Uall.append(u)
+                
     d = min(ranks)
 
     # Create a concatenated view of Us
     Uall_c = np.concatenate(Uall,axis=1)
 
     _,_,VV=svds(Uall_c,d)
-    VV = VV.T
+    VV = np.flip(VV.T,axis=1)
     VV = VV[:,:min([d,VV.shape[1]])]
 
     # SVDS the concatenated Us
@@ -58,10 +65,10 @@ def gcca(data, rank_tolerance=None, n_components=None):
     for i in range(len(data)):
         idx_start = idx_end
         idx_end = idx_start + ranks[i]
-        VVi = normalize(VV[idx_start:idx_end,:],'l2')
+        VVi = normalize(VV[idx_start:idx_end,:],'l2',axis=0)
         # Compute the canonical projections
-        A = np.sqrt(n-1) * Vall[i][:,:rank]
-        A = A @ (linalg.solve(np.diag(Sall[i][:rank]), VVi))
+        A = np.sqrt(n-1) * Vall[i][:,:ranks[i]]
+        A = A @ linalg.solve(np.diag(Sall[i][:ranks[i]]), VVi)
         projX.append(data[i] @ A)
-        
+                
     return(projX)
